1a18c99a19029a2f0e64a481c12de9e587fdc50d
Akmal Mustafa Ayyubi <59949866+Akmal4163@users.noreply.github.com>
2020-04-01 20:58:06 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,3 +2,23 @@ alert("halo dunia");
 var title = "belajar javascript";
 let visitor = 2243;
 
+var a = 3345;
+        var b = 3;
+        var c = 0;
+        c = a - b;
+        document.write(`${a} - ${b} = ${c}</br>`);
+        d = a * b;
+        document.write(`${a} * ${b} = ${d}</br>`);
+        e = a ** b;
+        document.write(`${a} ** ${b} = ${e}</br>`);
+        f = a / b;
+        document.write(`${a} / ${b} = ${f}</br>`);
+        g = a % b;
+        document.write(`${a} % ${b} = ${g}</br>`);
+
+var name = "Admin";
+var Jumlah_Kunjungan = 2243;
+var status = "aktif";
+alert("selamat datang " + name);
+document.write("Anda telah mengunjungi situs ini sebanyak " + Jumlah_Kunjungan + " kali" + "<br>" );
+document.write("status anda: " + status + "<br>" );
