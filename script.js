da6987dd955434b832fb5cf37bd43968c74f0c79
Akmal Mustafa Ayyubi <59949866+Akmal4163@users.noreply.github.com>
2020-09-19 20:47:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+sniper = {
+    nama: "sniper",
+    kemampuan: "menyerang",
+    kekuatan: 70,
+};
+rikimaru = {
+    nama: "rikimaru",
+    kemampuan: "bertahan",
+    kekuatan: 75
+};
+ucup = {
+    nama: "ucup",
+    kemampuan: "menyerang",
+    kekuatan: 90
+};
+otong = {
+    nama: "otong",
+    kemampuan: "bertahan",
+    kekuatan: 95
+};
